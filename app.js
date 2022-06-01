749c102b97215bc7cadbe163756e566b6cbc495b
ValeriaERodriguez <valeriaelisabetrgz@gmail.com>
2022-05-31 22:56:09 -0300

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var palabras = ['mesa','casa','radio','luna','HTML'];
+var palabras = ['MESA','CASA','RADIO','LUNA','HTML'];
 var tablero = document.getElementById('horca').getContext('2d');
 var letras = [];
 var intentos = 7
@@ -10,8 +10,46 @@ function seleccionarPalabra(){
     palabrasSecreta = palabra
     console.log(palabra)
     return palabra
-}seleccionarPalabra()
+}
 
+function dibujarLineas(){
+    tablero.lineWidth = 6
+    tablero.lineCap = "round"
+    tablero.lineJoin = "round"
+    tablero.strokeStyle = "#0A3871"
+    tablero.beginPath()
+
+    var ancho = 600/palabrasSecreta.length
+    for(let i = 0; i < palabrasSecreta.length; i++){
+        tablero.moveTo(500+(ancho*i),640)
+        tablero.lineTo(500+(ancho*i),640)
+    }
+    tablero.stroke()
+    tablero.closePath()
+}dibujarLineas(seleccionarPalabra());
+
+
+function escribirLetraCorrecta(index){
+    tablero.font = 'bold 52px';
+    tablero.lineWidth = 6
+    tablero.lineCap = "round"
+    tablero.lineJoin = "round"
+    tablero.fillStyle = "#0A3871"
+
+    var ancho = 600/palabrasSecreta.length
+    tablero.fillText(palabrasSecreta[index], 505+(ancho*index),620)
+}
+
+function escribirLetraIncorrecta(letra, fallos){
+    tablero.font = 'bold 40px';
+    tablero.lineWidth = 6
+    tablero.lineCap = "round"
+    tablero.lineJoin = "round"
+    tablero.fillStyle = "#0A3871"
+
+    tablero.fillText(letra, 535+(40*(10-fallos)), 710,40)
+
+}
 function probar(event){
     comprobarLetra(event.target.textContent);
 }
