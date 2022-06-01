2ec9edb2388b076aec565a995c16405a5118049b
ValeriaERodriguez <valeriaelisabetrgz@gmail.com>
2022-05-31 22:25:15 -0300

    .

--- app.js
+++ app.js
@@ -5,31 +5,18 @@ var intentos = 7
 var palabraCorrecta = "";
 var fallos = 9;
 
-function seleccionarPalabra (palabras){
-    var totalPalabras = palabras.length - 1;
-    var aleatoria = (Math.random() * totalPalabras).toFixed(0);
-    palabras = palabras[aleatoria].toLowerCase();
-    pintarPalabra(palabras);
-}
+function seleccionarPalabra(){
+    var palabra = palabras[Math.floor(Math.random()*palabras.length)]
+    palabrasSecreta = palabra
+    console.log(palabra)
+    return palabra
+}seleccionarPalabra()
 
 function probar(event){
     comprobarLetra(event.target.textContent);
 }
 
-function pintarPalabra(){
-    var str = "";
-    var letras = palabras.split("");
 
-    letras.forEach(l,1) => {
-        if (concordacia.includes(l)){
-            str +=
-        }else{
-            concordacia[i] = ´_´;
-
-        };
-    };
-    document.getElementById(palabras).innerHTML = str;
-}
 
 function abc(){
     var a = 97;
